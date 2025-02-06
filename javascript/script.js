08adab46a755d2a63c014cd539c3aec637ed0af8
zMiguel151z <pereira.miguel@escolar.ifrn.edu.br>
2025-02-06 16:10:22 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+
+function historia(){
+    document.getElementById("secao-1").style.display = "block";
+}
+
+function historia2(){
+    document.getElementById("secao-2").style.display = "block";
+}
+
+function historia3(){
+    document.getElementById("secao-3").style.display = "block";
+}
+
+function historia4(){
+    document.getElementById("secao-4").style.display = "block";
+}
